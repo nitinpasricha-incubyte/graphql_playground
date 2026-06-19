467075c3affedefb4c933fbd9fd6b0455f002ea8
Nitin Pasricha <nitin.p@incubyte.co>
2026-06-19 20:49:23 +0530

    added mutation for deleting user resource

---
+++ app/graphql/mutations/delete_user.rb
@@ -0,0 +1,18 @@
+module Mutations
+    class DeleteUser < BaseMutation
+        argument :id, ID
+
+        field :user, Types::UserType, null: true
+
+        def resolve(id:)
+            user = User.find(id)
+            raise GraphQL::ExecutionError, "User not found with id: #{id}" unless user
+
+            if user.destroy
+                { user: user }
+            else
+                raise GraphQL::ExecutionError, user.errors.full_messages.join(",")
+            end
+        end
+    end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,6 +4,7 @@ module Types
   class MutationType < Types::BaseObject
     field :create_user, mutation: Mutations::CreateUser
     field :update_user, mutation: Mutations::UpdateUser
+    field :delete_user, mutation: Mutations::DeleteUser
     # TODO: remove me
     # field :test_field, String, null: false,
     #   description: "An example field added by the generator"
