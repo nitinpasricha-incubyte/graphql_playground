c13c9c293b61f3979fbe89036de8f0bedb6fe71a
Nitin Pasricha <nitin.p@incubyte.co>
2026-06-22 14:21:51 +0530

    added specs for users GraphQL query

---
+++ spec/grahpql/queries/users_spec.rb
@@ -0,0 +1,48 @@
+require "rails_helper"
+
+RSpec.describe "Users Query" do
+
+  let(:query) do
+    <<-GQL
+      query {
+        users {
+          id
+          fullName
+          email
+        }
+      }
+    GQL
+  end
+
+  def execute_query
+    GraphqlPlaygroundSchema.execute(query).to_h
+  end
+
+  context "when there are no users" do
+    it "returns an empty list" do
+      result = execute_query
+
+      expect(result["data"]["users"]).to eq([])
+    end
+  end
+
+  context "when users exist" do
+    let!(:user1) { User.create!(first_name: "Nitin", last_name: "Pasricha", email: "nitin@pasricha.com") }
+    let!(:user2) { User.create!(first_name: "Jaymin", last_name: "Shah", email: "jaymin@shah.com") }
+
+    it "returns all users" do
+      result = execute_query
+
+      expect(result["data"]["users"].length).to eq(2)
+    end
+
+    it "returns correct fields for each user" do
+      result = execute_query
+
+      expect(result["data"]["users"]).to include(
+        { "id" => user1.id.to_s, "fullName" => "Nitin Pasricha", "email" => "nitin@pasricha.com" },
+        { "id" => user2.id.to_s, "fullName" => "Jaymin Shah", "email" => "jaymin@shah.com" }
+      )
+    end
+  end
+end
