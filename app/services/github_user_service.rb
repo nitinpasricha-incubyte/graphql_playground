294a4b9b610e074ab3d5b2baa2e40799352f4fa8
Nitin Pasricha <nitin.p@incubyte.co>
2026-06-23 12:22:08 +0530

    feat: implemented service GithubUserService and cassetes for its api results

---
+++ app/services/github_user_service.rb
@@ -0,0 +1,28 @@
+require 'net/http'
+require 'json'
+
+class GithubUserService
+  UserNotFoundError = Class.new(StandardError)
+
+  GITHUB_API_URL = 'https://api.github.com/users'
+
+  def initialize(username)
+    @username = username
+  end
+
+  def call
+    uri = URI("#{GITHUB_API_URL}/#{@username}")
+    response = Net::HTTP.get_response(uri)
+
+    raise UserNotFoundError, "GitHub user '#{@username}' not found" if response.code == '404'
+
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    {
+      login:        data[:login],
+      name:         data[:name],
+      avatar_url:   data[:avatar_url],
+      public_repos: data[:public_repos]
+    }
+  end
+end
